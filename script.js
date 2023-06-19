9ddb1d8266c3dbffd88ebd78c09df5051980eba3
ghubisz <gab.op.pl@op.pl>
2023-06-19 17:45:04 +0200

    3

--- script.js
+++ script.js
@@ -11,28 +11,42 @@ const questions = [
     {
         question: "Which is the largest animal in the world?",
         answers: [
-            { text: "Shark", coorect: false},
-            { text: "Blue whale", coorect:true},
-            { text: "Elephant", coorect: false},
+            { text: "Shark", corect: false},
+            { text: "Blue whale", corect:true},
+            { text: "Elephant", corect: false},
             { text: "Giraffe", correct: false},
         ]
     },
     {
-        question: "Which is the largest animal in the world?",
+        question: "Which is the largest desert in the world?",
         answers: [
-            { text: "Shark", coorect: false},
-            { text: "Blue whale", coorect:true},
-            { text: "Elephant", coorect: false},
-            { text: "Giraffe", correct: false},
+            { text: "Kalahari", corect: false},
+            { text: "Gobi", corect:true},
+            { text: "Sahara", corect: false},
+            { text: "Antarctica", correct: true},
         ]
     },
     {
-        question: "Which is the largest animal in the world?",
+        question: "Which is the smallest continent in the world?",
         answers: [
-            { text: "Shark", coorect: false},
-            { text: "Blue whale", coorect:true},
-            { text: "Elephant", coorect: false},
-            { text: "Giraffe", correct: false},
+            { text: "Asia", corect: false},
+            { text: "Australia", corect:true},
+            { text: "Arctic", corect: false},
+            { text: "Africa", correct: false},
         ]
     }
-]
+];
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
