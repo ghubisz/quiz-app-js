3d496d65922ddae3c0a1fe18ae0c4f6bd7606fd7
ghubisz <gab.op.pl@op.pl>
2023-06-19 15:41:23 +0200

    2

---
+++ script.js
@@ -0,0 +1,38 @@
+const questions = [
+    {
+        question: "Which is the largest animal in the world?",
+        answers: [
+            { text: "Shark", coorect: false},
+            { text: "Blue whale", coorect:true},
+            { text: "Elephant", coorect: false},
+            { text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question: "Which is the largest animal in the world?",
+        answers: [
+            { text: "Shark", coorect: false},
+            { text: "Blue whale", coorect:true},
+            { text: "Elephant", coorect: false},
+            { text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question: "Which is the largest animal in the world?",
+        answers: [
+            { text: "Shark", coorect: false},
+            { text: "Blue whale", coorect:true},
+            { text: "Elephant", coorect: false},
+            { text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question: "Which is the largest animal in the world?",
+        answers: [
+            { text: "Shark", coorect: false},
+            { text: "Blue whale", coorect:true},
+            { text: "Elephant", coorect: false},
+            { text: "Giraffe", correct: false},
+        ]
+    }
+]
